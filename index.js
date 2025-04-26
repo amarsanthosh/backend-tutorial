22f1d25a8e2d51bbfcf7c2efa345e80fd258430b
Amarsanthosh Santhosh <amarsanthoshsanthosh@gmail.com>
2025-04-26 09:46:27 +0530

    to create a server in Express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hi da....");
+const express = require('express');
+
+const app = express();
+app.get('/',(req,res)=>{
+    res.send("My App is working fine...");
+});
+const PORT = 3000;
+app.listen(PORT, () =>{
+    console.log("app is listeneing port no : " + PORT);
+}); 
